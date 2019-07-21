31d6dca9fc26745bc5c7b65b86901810a39b2863
David <dtfeldman@verion.net>
2019-07-20 20:35:35 -0400

    Updated Packages

--- Packages.py
+++ Packages.py
@@ -1,7 +1,9 @@
 import re
 import pandas
 from smtplib import SMTP
-# 07/16/2019
+
+
+# 07/20/2019
 
 
 def float_input():
@@ -71,15 +73,15 @@ def database(arr, titles=False):
                 indx = ["x", "y", "z"]
             else:
                 indx = range(1, len(arr) + 1)
-            cols = range(1, len(arr[0])+1)
+            cols = range(1, len(arr[0]) + 1)
 
         elif isinstance(arr[0], float):
             indx = ["y"]
-            cols = range(1, len(arr)+1)
+            cols = range(1, len(arr) + 1)
 
         else:
             indx = range(1, len(arr) + 1)
-            cols = range(1, len(arr[0])+1)
+            cols = range(1, len(arr[0]) + 1)
     else:
         indx = titles
         cols = range(1, len(arr[0]) + 1)
@@ -94,11 +96,11 @@ def time_delta_display(sec):
     :return:
     """
     # Conversion key
-    intervals = (('years', 31536000),   # 60 * 60 * 24 * 365
-                 ('months', 2628288),   # 60 * 60 * 24 * 30.42     (30.42 is the avg number of days in a month)
-                 ('weeks', 604800),     # 60 * 60 * 24 * 7
-                 ('days', 86400),       # 60 * 60 * 24
-                 ('hours', 3600),       # 60 * 60
+    intervals = (('years', 31536000),  # 60 * 60 * 24 * 365
+                 ('months', 2628288),  # 60 * 60 * 24 * 30.42     (30.42 is the avg number of days in a month)
+                 ('weeks', 604800),  # 60 * 60 * 24 * 7
+                 ('days', 86400),  # 60 * 60 * 24
+                 ('hours', 3600),  # 60 * 60
                  ('minutes', 60),
                  ('seconds', 1),)
     result = []
@@ -126,7 +128,7 @@ def display_num(num):
     :param num:
     :return:
     """
-    if 1 <= num / (1*10**3) < 1000:
+    if 1 <= num / (1 * 10 ** 3) < 1000:
         str_num = str(round(num, 2))
         r_str_num = str_num[::-1]
         if '.' in r_str_num:
@@ -136,37 +138,78 @@ def display_num(num):
         o = r_str_num[:start_position + 4] + "," + r_str_num[start_position + 4:]
         return o[::-1]
 
-    elif 1 <= num / (1*10**6) < 1000:
-        return str(round(num/(1*10**6), 2)) + " million"
+    elif 1 <= num / (1 * 10 ** 6) < 1000:
+        return str(round(num / (1 * 10 ** 6), 2)) + " million"
 
-    elif 1 <= num / (1*10**9) < 1000:
-        return str(round(num/(1*10**9), 2)) + " billion"
+    elif 1 <= num / (1 * 10 ** 9) < 1000:
+        return str(round(num / (1 * 10 ** 9), 2)) + " billion"
 
-    elif 1 <= num / (1*10**12) < 1000:
-        return str(round(num/(1*10**12), 2)) + " trillion"
+    elif 1 <= num / (1 * 10 ** 12) < 1000:
+        return str(round(num / (1 * 10 ** 12), 2)) + " trillion"
 
-    elif 1 <= num / (1*10**15) < 1000:
-        return str(round(num/(1*10**15), 2)) + " quadrillion"
+    elif 1 <= num / (1 * 10 ** 15) < 1000:
+        return str(round(num / (1 * 10 ** 15), 2)) + " quadrillion"
 
-    elif 1 <= num / (1*10**18) < 1000:
-        return str(round(num/(1*10**18), 2)) + " quintillion"
+    elif 1 <= num / (1 * 10 ** 18) < 1000:
+        return str(round(num / (1 * 10 ** 18), 2)) + " quintillion"
 
-    elif 1 <= num / (1*10**21) < 1000:
-        return str(round(num/(1*10**21), 2)) + " sextillion"
+    elif 1 <= num / (1 * 10 ** 21) < 1000:
+        return str(round(num / (1 * 10 ** 21), 2)) + " sextillion"
 
     else:
         return str(num)
 
 
-def send_sms(message):
+def send_sms(message, number='813-352-2669', carrier='verizon'):
+    # Cannot send non alphanumeric characters (ex. ":)")
+    # TODO: Upgrade with Gmail API
     carriers = {'att': '@mms.att.net',
-                'tmobile': '@tmomail.net',
-                'verizon': '@vtext.com',
-                'sprint': '@page.nextel.com'}
+                'tmobile': '@tmomail.net',  # Not working , blocking MX, DNS mismatch
+                'verizon': '@vtext.com',  # Working
+                'sprint': '@messaging.sprintpcs.com',  # Working
+                'at&t': '@txt.att.net',
+                'boost': '@myboostmobile.com',
+                'cricket': '@sms.mycricket.com',
+                'metropcs': '@mymetropcs.com',
+                'tracfone': '@mmst5.tracfone.com',
+                'uscell': '@email.uscc.net',
+                'virgin': '@vmobl.com'}
+    # BUILD THE MESSAGE
+    # Removes all formatting from the phone number
+    number = number.replace('-', '').replace('(', '').replace(')', '')
+    # Adds the carrier extention to the phone number
+    to_number = number + '{}'.format(carriers[carrier])
+    # Sets the id and password to the email acct
+    auth = ('dfeldmansfakeemail@gmail.com', privacy_decoder('ÜÐÚÚÜÓºâÊíâå', "password"))
+    # Refuses the message if the length is too long
+    if len(message) > 128:
+        print("Message too long (>128)")
+        return "Message too long (>128)"
 
-    # Replace the number with your own, or consider using an argument\dict for multiple people.
-    to_number = '000-000-0000{}'.format(carriers['att'])
-    auth = ('**email**', '**password**')
+    # SEND THE MESSAGE
+    # Establish a secure session with gmail's outgoing SMTP server using your gmail account
+    server = SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(auth[0], auth[1])
+    # Send text message through SMS gateway of destination number
+    server.sendmail(auth[0], to_number, message)
+    server.quit()
+    print("Sent SMS")
+    """
+    import Package
+
+    some_text = 'Blah, blah'
+
+    Package.send(some_text, number, carrier)
+
+    find any number's carrier using https://freecarrierlookup.com/
+    """
+    return "Sent SMS"
+
+
+def send_email(message, address='dtfeldman@verizon.net'):
+    # login to the gmail acct
+    auth = ('dfeldmansfakeemail@gmail.com', privacy_decoder('ÜÐÚÚÜÓºâÊíâå', "password"))
 
     # Establish a secure session with gmail's outgoing SMTP server using your gmail account
     server = SMTP("smtp.gmail.com", 587)
@@ -174,12 +217,35 @@ def send_sms(message):
     server.login(auth[0], auth[1])
 
     # Send text message through SMS gateway of destination number
-    server.sendmail(auth[0], to_number, message)
+    # server.sendmail(auth[0], to_number, message)
+    server.sendmail(auth[0], address, message)
+    server.quit()
 
+
+def privacy_encoder(message, key):
+    encoded_chars = []
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(message[i]) + ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    return "".join(encoded_chars)
+
+
+def privacy_decoder(encoded_message, key):
+    decoded_chars = []
+    for i in range(len(encoded_message)):
+        key_c = key[i % len(key)]
+        decoded_c = chr(ord(encoded_message[i]) - ord(key_c) % 256)
+        decoded_chars.append(decoded_c)
+    return ''.join(decoded_chars)
+
+
+def string_chunker(strin, char_num):
+    return [strin[i:i + char_num] for i in range(0, len(strin), char_num)]
+
+
+if __name__ == '__main__':
     """
-    import SMS
-
-    some_text = 'Blah, blah'
-
-    SMS.send(some_text)
+    for x in dns.resolver.query('gmail.com', 'MX'):
+        print(x.to_text())
     """
